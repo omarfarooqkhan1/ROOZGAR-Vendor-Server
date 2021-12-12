a3edc5df42a0e24181bf605659854e1ce8d9e2df
Omar Farooq <omarfarooqkhan@outlook.com>
2021-12-11 20:36:02 -0800

    added start script

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const { mongoUrl } = require("./keys");
 require("./models/Vendor");
 require("./models/Client");
@@ -36,7 +36,7 @@ mongoose.connection.on("error", (err) => {
   console.log("This is error", err);
 });
 
-var server = app.listen(port, () => {
-  var port = server.address().port;
+var server = app.listen(PORT, () => {
+  var port = server.address().PORT;
   console.log("Server is running on PORT: " + port);
 });
