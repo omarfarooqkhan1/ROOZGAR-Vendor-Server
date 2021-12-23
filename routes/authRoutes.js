27ac9dd60ca60a43226e79f3b129587b092db971
Omar Farooq <omarfarooqkhan@outlook.com>
2021-12-23 10:25:46 -0800

    ngrok

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -339,7 +339,7 @@ router.get("/getReviews/:_id", async (req, res) => {
 
     let payload = { sentences: comments };
 
-    let response = await axios.post('http://18bc-37-111-134-191.ngrok.io/sentiment', payload);
+    let response = await axios.post('http://93a0-103-255-7-49.ngrok.io/sentiment', payload);
     
     const sentiment = response.data.sentiment;
     
@@ -417,7 +417,7 @@ router.get("/getSubCategories/", async (req, res) => {
     }
     let payload = { sentence: "I need to mow my garden", categories: subCategoryNames, threshold: 3 };
 
-    let response = await axios.post('http://18bc-37-111-134-191.ngrok.io/nlp', payload);
+    let response = await axios.post('http://93a0-103-255-7-49.ngrok.io/nlp', payload);
     
     var similar_subCategories = response.data.similar_subCategories;
 
